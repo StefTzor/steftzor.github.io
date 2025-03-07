d171157941b0fb30d702f5a820610290fb941fe8
Stefanos Tzortzoglou <steve.tzortz@hotmail.com>
2025-03-07 13:55:12 +0100

    Fixing Eleventy deployment with Nunjucks

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,13 @@
 module.exports = function(eleventyConfig) {
   // Copy the dist folder (Tailwind output) to the final site
   eleventyConfig.addPassthroughCopy("dist");
+  eleventyConfig.addPassthroughCopy("scripts");
+  eleventyConfig.addPassthroughCopy("images");
 
   return {
     dir: {
       input: "pages",          // Source directory for pages
-      includes: "../_includes", // Folder for partials like headers/footers
+      includes: "_includes", // Folder for partials like headers/footers
       output: "_site",         // Build output folder (for GitHub Pages)
     },
     pathPrefix: "/", // Important: Set pathPrefix to `/` for GitHub Pages root deployment
