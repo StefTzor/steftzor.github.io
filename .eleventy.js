8c80da37a52272898be08964db2bc68ed83dc4cd
Stefanos Tzortzoglou <34420752+StefTzor@users.noreply.github.com>
2025-03-07 12:02:46 +0100

    Fixing gh-pages deployment

--- .eleventy.js
+++ .eleventy.js
@@ -2,14 +2,12 @@ module.exports = function(eleventyConfig) {
   // Copy the dist folder (Tailwind output) to the final site
   eleventyConfig.addPassthroughCopy("dist");
 
-  // Add a global baseUrl variable to handle GitHub Pages URLs
-  eleventyConfig.addGlobalData("baseUrl", process.env.GITHUB_URL || "/");
-
   return {
     dir: {
       input: "pages",          // Source directory for pages
       includes: "../_includes", // Folder for partials like headers/footers
       output: "_site",         // Build output folder (for GitHub Pages)
     },
+    pathPrefix: "/", // Important: Set pathPrefix to `/` for GitHub Pages root deployment
   };
 };
