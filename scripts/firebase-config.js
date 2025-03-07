753a9512860ebe2cd8726223d1ed3bdddd4c0849
StefTzor <StefTzor@users.noreply.github.com>
2025-03-07 15:01:19 +0000

    deploy: cb4012b2efe91a770ba563c89757f7496f887837

--- scripts/firebase-config.js
+++ scripts/firebase-config.js
@@ -1,18 +1,22 @@
+// Import Firebase functions
 import { initializeApp } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/11.3.1/firebase-firestore.js";
 
+// Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBqT5tJFXj6HG0sSHvnMheKCFhL9e_zh58",
-  authDomain: "mypersonalsite-79ce4.firebaseapp.com",
-  projectId: "mypersonalsite-79ce4",
-  storageBucket: "mypersonalsite-79ce4.firebasestorage.app",
-  messagingSenderId: "22661264733",
-  appId: "1:22661264733:web:6dd332cad11704c57864b4"
+  apiKey: "{{FIREBASE_API_KEY}}",
+  authDomain: "{{FIREBASE_AUTH_DOMAIN}}",
+  projectId: "{{FIREBASE_PROJECT_ID}}",
+  storageBucket: "{{FIREBASE_STORAGE_BUCKET}}",
+  messagingSenderId: "{{FIREBASE_MESSAGING_SENDER_ID}}",
+  appId: "{{FIREBASE_APP_ID}}"
 };
-
+  
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
+// Export the initialized instances so other scripts can use them
 export { auth, db };
