778bd733acd155d068d53fcdefd3987a913d6d5a
Stefanos Tzortzoglou <steve.tzortz@hotmail.com>
2025-03-07 11:54:54 +0100

    Changing .eleventy.js config with baseUrl

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,15 @@
 module.exports = function(eleventyConfig) {
-    // Copy Tailwind output folder to the final site
-    eleventyConfig.addPassthroughCopy("dist");
-  
-    return {
-      dir: {
-        input: "pages",         // Source directory for pages
-        includes: "../_includes", // Folder for partials like headers/footers
-        output: "_site",         // Build output folder (for GitHub Pages)
-      },
-    };
+  // Copy the dist folder (Tailwind output) to the final site
+  eleventyConfig.addPassthroughCopy("dist");
+
+  // Add a global baseUrl variable to handle GitHub Pages URLs
+  eleventyConfig.addGlobalData("baseUrl", process.env.GITHUB_URL || "/");
+
+  return {
+    dir: {
+      input: "pages",          // Source directory for pages
+      includes: "../_includes", // Folder for partials like headers/footers
+      output: "_site",         // Build output folder (for GitHub Pages)
+    },
   };
-  
+};
